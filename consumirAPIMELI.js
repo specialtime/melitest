99ed82a3e690bc49317bcb85e48634ea4595d07f
Cristian Portillo <you@example.com>
2020-03-28 17:47:41 -0300

    new

---
+++ consumirAPIMELI.js
@@ -0,0 +1,33 @@
+'use strict'
+const fetch = require('cross-fetch');
+const fs = require('fs');
+const readlineSync = require('readline-sync');
+
+var sellerID = JSON.parse("["+ readlineSync.question('Ingrese el ID de Seller: ')+"]");
+var urlCategory = "https://api.mercadolibre.com/categories/";
+var items = [];
+var categoryName = [];
+
+function invocarAPI(element) {
+    var cantItem = 0;
+    var urlItem = "https://api.mercadolibre.com/sites/MLA/search?seller_id="+element;
+    fetch(urlItem)
+    .then(data => data.json())
+    .then(data => {
+        items = data.results;
+        items.forEach(results=>{
+            cantItem = cantItem + 1
+            fetch(urlCategory+results.category_id)
+            .then (data => data.json())
+            .then(data =>{
+                categoryName = data.name;
+                fs.appendFile('log.txt',results.id+","+results.title+","+results.category_id+","+categoryName+"\n" , function (err) {
+                    if (err) throw err;
+                }); 
+            })
+        }) 
+        console.log("Se guardaron "+cantItem+" items en log.txt");
+    });
+}
+
+sellerID.forEach(invocarAPI);
